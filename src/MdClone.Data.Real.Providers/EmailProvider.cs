c91c472ec3bd22e2afd375a17dedd4e327a03a89
David Kossoglyad <david@logoui.co.il>
2021-07-13 14:18:29 +0300

    fix: make class sealed

--- src/MdClone.Data.Real.Providers/EmailProvider.cs
+++ src/MdClone.Data.Real.Providers/EmailProvider.cs
@@ -1,9 +1,11 @@
 ﻿using System.Threading;
+using JetBrains.Annotations;
 using MdClone.Data.Contracts.Dto;
 using MdClone.Data.Contracts.Providers;
 
 namespace MdClone.Data.Real.Providers
 {
+    [UsedImplicitly]
     internal sealed class EmailProvider : IEmailProvider
     {
         public void SendEmail(EmailDto emailDto)
